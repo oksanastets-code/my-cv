02ca7a2a1cc7e10b4a805a6299d63c14b5d905c3
Oksana Stets <oksanastets73@gmail.com>
2022-06-30 12:39:25 +0300

    complete contacts section

--- src/components/Sidebar/ContactsBlock/ContactsBlock.jsx
+++ src/components/Sidebar/ContactsBlock/ContactsBlock.jsx
@@ -1,44 +1,60 @@
 import styled from 'styled-components';
 import sprite from '../../../img/sprite.svg';
 
-export const ContactBlock = ({ items }) => (
-  <Block>
-    <Title>Contacts</Title>
-    <ul>
-      {items.map(item => (
-        <ContactItem key={item.id}>
-          <a href={item.href}>
-            <Icon width="16" height="1rem">
-              <use href={`${sprite}{item.icon}`}></use>
-            </Icon>
-            {item.address}
-          </a>
-        </ContactItem>
-      ))}
-    </ul>
-    <address>
-      {/* <svg>
-        <use href="../img/sprite.svg#icon-icon-location"></use>
-      </svg> */}
-      Lviv, Ukraine
-    </address>
-  </Block>
-);
+export const ContactBlock = ({ items }) => {
+  return (
+    <Block>
+      <Title>Contacts</Title>
+      <ul>
+        {items.map(item => (
+          <ContactItem key={item.id}>
+            {!item.address ? (
+              <Link href={item.href}>
+                <Icon width="16" height="1rem">
+                  <use href={`${sprite}` + item.icon}></use>
+                </Icon>
+                {item.point}
+              </Link>
+            ) : (
+              <Address>
+                <Link href={item.href}>
+                  <Icon width="16" height="1rem">
+                    <use href={`${sprite}` + item.icon}></use>
+                  </Icon>
+                  {item.point}
+                </Link>
+              </Address>
+            )}
+          </ContactItem>
+        ))}
+      </ul>
+    </Block>
+  );
+};
 
-const Block = styled.div`
-`;
+const Block = styled.div``;
 const Title = styled.h3`
   font-size: 22px;
   line-height: 27px;
   margin-bottom: 10px;
 `;
 const ContactItem = styled.li`
-   display: flex;
-  align-items: center;
   margin-bottom: 8px;
   font-size: 16px;
   line-height: 24px;
-`
+`;
 const Icon = styled.svg`
   fill: ${p => p.theme.colors.secondary};
+  margin-right: 10px;
+`;
+const Address = styled.address`
+  font-style: normal;
+`
+const Link = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  &:hover {
+  color: ${p => p.theme.colors.secondary};
+  }
 `
