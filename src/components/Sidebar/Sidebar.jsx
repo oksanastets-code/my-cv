1e203bdff7eb4c60f56bce2ac2bbdf5a264544fc
Oksana Stets <oksanastets73@gmail.com>
2022-06-30 18:15:24 +0300

    format sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -12,7 +12,7 @@ import languages from '../../data/languages.json';
 export const Sidebar = () => {
   return (
     <AsideBar>
-      <Photo/>
+      <Photo />
       <ContactBlock items={contacts} />
       <Skills items={skills} />
       <Languages title={languages.title} stack={languages.stack} />
@@ -27,5 +27,16 @@ const AsideBar = styled.aside`
   justify-content: space-between;
   padding: 40px 35px 40px 35px;
   color: ${p => p.theme.colors.textSidebar};
-  background-color: #1e2939;
+  /* background-color: #1e2939; */
+  background: #1e2939; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to top,
+    #1e2939,
+    rgba(30, 41, 57, 0.5)
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to top,
+    #1e2939,
+    rgba(30, 41, 57, 0.5)
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 `;
