9a7841ce75857b32ad8be6087fa664b4c1ceb30f
Oksana Stets <oksanastets73@gmail.com>
2022-10-27 23:39:29 +0300

    bugfix, extract courses from education

--- src/components/MainContant/Education/Education.jsx
+++ src/components/MainContant/Education/Education.jsx
@@ -4,23 +4,45 @@ import { ContainerBlock } from '../ContainerBlock';
 import { EducationItem } from './EducationItem';
 
 export const Education = ({ items }) => (
-  <ContainerBlock name = {'Education'}>
-  <ul>
-    {items.map(item => (
-      <EduBlock key={item.id}>
-        <EducationItem
-          school={item.school}
-          qualification={item.qualification}
-          period={item.period}
-          place={item.place}
-        />
-      </EduBlock>
-    ))}
-  </ul>
-   </ContainerBlock>
+  <>
+    <ContainerBlock name={'Education'}>
+      <ul>
+        {items.map(
+          item =>
+            item.id === '2' && (
+              <EduBlock key={item.id}>
+                <EducationItem
+                  school={item.school}
+                  qualification={item.qualification}
+                  period={item.period}
+                  place={item.place}
+                />
+              </EduBlock>
+            )
+        )}
+      </ul>
+    </ContainerBlock>
+    <ContainerBlock name={'Courses'}>
+       <ul>
+      {items.map(
+        item =>
+          item.id === '1' && (
+            <EduBlock key={item.id}>
+              <EducationItem
+                school={item.school}
+                qualification={item.qualification}
+                period={item.period}
+                place={item.place}
+              />
+            </EduBlock>
+          )
+      )}
+        </ul>
+    </ContainerBlock>
+  </>
 );
 const EduBlock = styled.li`
   &:not(:last-child) {
     margin-bottom: 10px;
   }
-`
+`;
